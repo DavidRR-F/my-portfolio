4941b75a1633b70785c96e704213495a6c37eb54
DavidRR-F <david.rosefranklin96@gmail.com>
2023-06-17 08:45:01 -0400

    sidebar animation

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,9 @@
+"use client";
+import { useStateContext } from "@/components/Providers";
 import CanvasModel from "@/components/canvas/CanvasModel";
 
 export default function Home() {
+  const { activeMenu } = useStateContext();
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <CanvasModel />
--- components/navigation/Sidebar.tsx
+++ components/navigation/Sidebar.tsx
@@ -1,17 +1,18 @@
 "use client";
 
 import { useStateContext } from "../Providers";
+import NavItems from "./NavItems";
 
 const Sidebar = () => {
   const { activeMenu } = useStateContext();
   return (
     <>
       {activeMenu ? (
-        <div className="w-72 fixed dark:bg-secondary-dark-bg bg-white">
-          Sidebar
+        <div className="w-72 h-full fixed dark:bg-secondary-dark-bg bg-light-primary-2 transition-all z-10">
+          <NavItems />
         </div>
       ) : (
-        <div className="w-0 dark:bg-secondary-dark-bg"></div>
+        <div className="w-0 h-full fixed bg-light-primary-2 dark:bg-secondary-dark-bg transition-all z-10"></div>
       )}
     </>
   );
